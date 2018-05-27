ce9daa1964af559111e858c9a3d7d9a13dd14931
Lorenzo Soligo <lorenzo.a7x@gmail.com>
2018-05-27 17:57:56 +0200

    svolta query 5

--- queries/05-mongo-embedded.js
+++ queries/05-mongo-embedded.js
@@ -1,1 +1,41 @@
 // 5. Find all the countries that have not traded *010600* or *010519* in 1998
+
+var countries_that_traded = (db.trades.aggregate([
+    {
+        $match : {
+            "year" : "1998",
+            "commodity.code" : {
+                $in : ["010600", "010519"]
+            } 
+        }
+    },
+    {
+        $group : {
+            _id : 0,
+            "countries" : {
+                $addToSet: "$country_or_area"
+            } 
+        }
+    }
+]).toArray())[0].countries
+
+
+var countries_didnt_trade = (db.trades.aggregate([
+    {
+        $match : {
+            "country_or_area" : {
+                $nin : countries_that_traded
+            }
+        }
+    },
+    {
+        $group : {
+            _id : 0,
+            "countries" : {
+                $addToSet : "$country_or_area"
+            }
+        }
+    }
+]).toArray())[0].countries
+
+printjson(countries_didnt_trade)
--- queries/05-mongo-reference.js
+++ queries/05-mongo-reference.js
@@ -1,2 +1,58 @@
 // 5. Find all the countries that have not traded *010600* or *010519* in 1998
 
+// 5. Find all the countries that have not traded *010600* or *010519* in 1998
+
+var countries_that_traded = (db.commodities_ref.aggregate([
+    {
+        $match : {
+            "code" : {
+                $in : ["010600", "010519"]
+            } 
+        }
+    },
+    {
+        $lookup : {
+            from : "trades_ref",
+            localField : "code",
+            foreignField : "comm_code",
+            as : "commodities_trades"
+        }
+    },
+    {
+        $unwind : "$commodities_trades"
+    },
+    {
+        $match : {
+            "commodities_trades.year" : "1998"
+        }
+    },
+    {
+        $group : {
+            _id : 0,
+            "countries" : {
+                $addToSet: "$commodities_trades.country_or_area"
+            } 
+        }
+    }
+]).toArray())[0].countries
+
+
+var countries_didnt_trade = (db.trades_ref.aggregate([
+    {
+        $match : {
+            "country_or_area" : {
+                $nin : countries_that_traded
+            }
+        }
+    },
+    {
+        $group : {
+            _id : 0,
+            "countries" : {
+                $addToSet : "$country_or_area"
+            }
+        }
+    }
+]).toArray())[0].countries
+
+printjson(countries_didnt_trade)
